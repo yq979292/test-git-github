58bc31898c8066e45f27ec42d0c41c7f32bef8be
yq979292 <319281998@qq.com>
2020-04-22 10:49:59 +0800

    fzn新建任务修改hcq的代码

---
+++ fzn.js
@@ -0,0 +1,4 @@
+// fzn 自己的任务文件；
+function z(){
+    console.log('这是我自己的工作任务')
+}
---
+++ hcq.js
@@ -0,0 +1,9 @@
+// 我是hcq 分支新建的文件
+function hcq(){
+    console.log('hcq编写code')
+}
+
+// fzn 分支修改了hcq 编写的带啊没
+function fzn(){
+    console.log('aaaa')
+}
